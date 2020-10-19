7ca0fc57c5524204dd65a9c62063d62fcada7b91
Gary Lai <garylai1990@gmail.com>
2020-10-19 17:58:34 +0800

    chore(config): enable gatsby plugin offline

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -4,7 +4,6 @@ import Layout from "../components/layout"
 
 const NotFoundPage = () => (
   <Layout>
-    <Seo title="404: Not found" />
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
