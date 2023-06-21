f055fd54cad2a87459fe12b11d7416b35a9e0fac
stanley-yin <stanley6140992@gmail.com>
2023-06-21 15:22:18 +0800

    feat: set trailing slash default to never

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     language: "zh-TW",
   },
+  trailingSlash: "never",
   plugins: [
     `gatsby-plugin-next-seo`,
     {
